f43dd88374cda280b8a7efc78c3bcda6328ed2d3
Dima Chavro <dchavro@gmail.com>
2025-03-22 18:12:03 +0000

    start working on landing

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,38 +1,75 @@
-export default function Home() {
-  const colors = [
-    "#c4d5bf",
-    "#b6dbb8",
-    "#a0ceaf",
-    "#a3d6c7",
-    "#75b1a7",
-    "#5fa08a",
-    "#498f6e",
-    "#326950",
-    "#1c5242",
-    "#053c34",
-  ];
-  return (
-    <div>
-      <h1>Welcome</h1>
+import { Paper, Stack, Flex, Title, Box } from "@mantine/core";
 
-      <p>Welcome to the React ❤️ django-allauth.</p>
-      <div
+export default function Home() {
+  return (
+    <Flex
+      gap="md"
+      justify="center"
+      align="center"
+      direction="column"
+      style={{ height: "calc(100vh - 130px)" }}
+    >
+      <Paper
+        w={{ base: "300px", sm: "400px" }}
+        radius="xs"
+        p="md"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-          marginTop: "20px",
+          backgroundColor: "rgba(196,213,191,255)",
+          // border: "1px sold #073b31",
         }}
+        withBorder
       >
-        {colors.map((color, index) => (
-          <div
-            key={index}
-            style={{ backgroundColor: color, height: "50px", width: "100px" }}
+        <Stack align="center">
+          <Title
+            order={4}
+            style={{
+              color: "#073b31",
+              textAlign: "center",
+            }}
           >
-            {color}
-          </div>
-        ))}
-      </div>
-    </div>
+            🔔 Get a Telegram message when your item pops up on freecycle.org
+          </Title>
+          <Stack gap={0}>
+            <img
+              src="/logo_main.svg"
+              alt=""
+              style={{
+                width: "250px",
+                height: "120px",
+              }}
+            ></img>
+            <img
+              src="/name_header_cropped.svg"
+              alt=""
+              style={{
+                width: "250px",
+                height: "80px",
+              }}
+            ></img>
+          </Stack>
+          <Box
+            my={0}
+            style={{
+              width: "120px",
+              border: "2px solid #073b31",
+            }}
+          />
+          <a
+            href="https://www.freecycle.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/freecycle.png"
+              alt="Freecycle Logo"
+              style={{
+                width: "280px",
+                height: "140px",
+              }}
+            />
+          </a>
+        </Stack>
+      </Paper>
+    </Flex>
   );
 }
