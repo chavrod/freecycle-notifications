539a2c78805219d9c59a86fe97f278087de4c3c9
Dima Chavro <dchavro@gmail.com>
2025-03-28 08:35:45 +0000

    Add sentry to the fronted project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
+
 import App from "./App.tsx";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import dns from "dns";
 import react from "@vitejs/plugin-react";
@@ -6,9 +7,17 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "dchavro",
+    project: "pingcycle-frontend"
+  })],
+
   server: {
     host: "127.0.0.1",
     port: 3000,
   },
+
+  build: {
+    sourcemap: true
+  }
 });
