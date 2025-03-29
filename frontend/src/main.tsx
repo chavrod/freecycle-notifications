027f2e5aac10c42ae76f586bc740f2be233a5773
Dima Chavro <dchavro@gmail.com>
2025-03-29 09:09:35 +0000

    Do not send sentry errors in dev

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,10 @@ import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dsn:
+    import.meta.env.VITE_ENV !== "DEV"
+      ? import.meta.env.VITE_SENTRY_DSN
+      : undefined,
 });
 
 createRoot(document.getElementById("root")!).render(
