689a20ac5426c1cfd061050b8e1d9e3611d1eafc
Dima Chavro <dchavro@gmail.com>
2025-04-09 09:22:25 +0100

    collect static fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,6 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 import json
 from pathlib import Path
 
@@ -195,7 +196,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
